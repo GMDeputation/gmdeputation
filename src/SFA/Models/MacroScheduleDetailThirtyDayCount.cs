341bd3faedc328a551b82f4c5a7a73fb4b226192
troytreynolds <troytreynolds@gmail.com>
2023-06-29 23:35:40 -0400

    Adding Views

---
+++ src/SFA/Models/MacroScheduleDetailThirtyDayCount.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SFA.Models
+{
+    public class MacroScheduleDetailThirtyDayCount
+    {
+        public int upcoming_mac_sched { get; set; }
+        public int upcoming_services { get; set; }
+
+    }
+   
+}
---
+++ src/SFA/Models/ServiceCountOneYear.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SFA.Models
+{
+    public class ServiceCountOneYear
+    {
+        public int cy_tot_services { get; set; }
+        public int py_tot_services { get; set; }
+        public string pct_yoy { get; set; }
+    }
+   
+}
